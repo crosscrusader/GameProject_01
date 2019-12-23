ea7ea67d866ad3aeb8e81451faab715fdfb5e55f
crosscrusader <izrahiahx@gmail.com>
2019-12-22 20:07:23 -0500

    The program can now create windows.

--- Game01/Game01/main.cpp
+++ Game01/Game01/main.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
+
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+void proccessInput(GLFWwindow * window);
+
+unsigned int SCREEN_WIDTH = 800;
+unsigned int SCREEN_HEIGHT = 600;
+
 int main() {
-	int a = glfwInit();
-	std::cout << a;
-	return a;
+	
+	// Initializes glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Makes window
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Ebic game", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	if (window == NULL) 
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Sets up rendering viewport
+	glfwMakeContextCurrent(window);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Initializes GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Main rendering loop
+	while (!glfwWindowShouldClose(window)) {
+
+		glClearColor(0.0, 0.0, 0.0, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Ends glfw
+	glfwTerminate();
+	return 0;
+
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void proccessInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
